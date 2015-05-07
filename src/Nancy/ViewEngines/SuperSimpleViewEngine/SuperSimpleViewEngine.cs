34d53cff65362feb8cb2da60d277492546573001
Kobi <kobikobi@gmail.com>
2015-05-07 10:58:38 +0300

    SSVE - AttributeValuePathExpansionRegEx corrected ["|'] to ["']
    AttributeValuePathExpansionRegEx allowed attributes like
    `attribute=|~path|`. Fixed Regex typo `["|']` to `["']`.

--- src/Nancy/ViewEngines/SuperSimpleViewEngine/SuperSimpleViewEngine.cs
+++ src/Nancy/ViewEngines/SuperSimpleViewEngine/SuperSimpleViewEngine.cs
@@ -76,6 +76,7 @@ namespace Nancy.ViewEngines.SuperSimpleViewEngine
         /// Compiled RegEx for path expansion in attribute values
         /// </summary>
         private static readonly Regex AttributeValuePathExpansionRegEx = new Regex(@"(?<Attribute>[a-zA-Z]+)=(?<Quote>[""|'])(?<Path>~.+?)\k<Quote>", RegexOptions.Compiled);
+        private static readonly Regex AttributeValuePathExpansionRegEx = new Regex(@"(?<Attribute>[a-zA-Z]+)=(?<Quote>[""'])(?<Path>~.+?)\k<Quote>", RegexOptions.Compiled);
 
         /// <summary>
         /// Compiled RegEx for the CSRF anti forgery token
